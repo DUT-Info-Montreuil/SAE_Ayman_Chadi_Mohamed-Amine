ada068c7c56ed781029fd58c9e84a47f8d26c066
Chadi99 <celkhouly@b114-12.iut.univ-paris8.fr>
2025-06-17 11:49:21 +0200

    ajout base du sorcier dans le controller

--- src/main/java/fr/mbouklikha/dev/sae_glacium/controller/Controller.java
+++ src/main/java/fr/mbouklikha/dev/sae_glacium/controller/Controller.java
@@ -1,9 +1,11 @@
 package fr.mbouklikha.dev.sae_glacium.controller;
 
 import fr.mbouklikha.dev.sae_glacium.modeles.acteur.Sid;
+import fr.mbouklikha.dev.sae_glacium.modeles.acteur.Sorcier;
 import fr.mbouklikha.dev.sae_glacium.modeles.acteur.Yeti;
 import fr.mbouklikha.dev.sae_glacium.modeles.monde.Environnement;
 import fr.mbouklikha.dev.sae_glacium.vues.acteur.SidVue;
+import fr.mbouklikha.dev.sae_glacium.vues.acteur.SorcierVue;
 import fr.mbouklikha.dev.sae_glacium.vues.acteur.YetiVue;
 import fr.mbouklikha.dev.sae_glacium.vues.monde.TerrainVue;
 
@@ -37,22 +39,33 @@ public class Controller {
     private Yeti yeti;
     private YetiVue yetiVue;
 
+    private Sorcier sorcier;
+    private SorcierVue sorcierVue;
+
     private final int TAILLE_BLOC = 32;
 
     @FXML
     public void initialize() {
+        // Création de l'environnement et du terrain
         this.env = new Environnement(992, 576);
         new TerrainVue(env.getTerrain(), tilePane);
 
+        // Création de Sid et sa vue
         sid = new Sid(env);
         sidVue = new SidVue(sid, zoneJeu);
 
+        // Création du Yeti et sa vue
         yeti = new Yeti(env, sid);
         yetiVue = new YetiVue(yeti, zoneJeu);
+
+        // Création du Sorcier et sa vue
+        sorcier = new Sorcier(env, sid);
+        sorcierVue = new SorcierVue(sorcier, zoneJeu);
 
         tilePane.setFocusTraversable(false);
         zoneJeu.setFocusTraversable(true);
 
+        // Initialisation des touches et démarrage de la boucle de jeu
         Platform.runLater(() -> {
             zoneJeu.setOnKeyPressed(event -> touchesActives.add(event.getCode()));
             zoneJeu.setOnKeyReleased(event -> touchesActives.remove(event.getCode()));
@@ -68,13 +81,19 @@ public class Controller {
         gameLoop.setCycleCount(Timeline.INDEFINITE);
 
         KeyFrame keyFrame = new KeyFrame(
-                Duration.seconds(0.017), // ≈ 60 FPS
+                Duration.seconds(0.017), // environ 60 FPS
                 ev -> {
+                    // Mise à jour Yeti
                     yeti.appliquerGravite(env.getTerrain().getMap(), TAILLE_BLOC);
                     yeti.suivreEtFrapperSid();
 
+                    // Mise à jour Sid
                     sid.deplacer(touchesActives);
                     sid.appliquerGravite(env.getTerrain().getMap(), TAILLE_BLOC);
+
+                    // Mise à jour Sorcier
+                    sorcier.deplacer(touchesActives);
+                    sorcier.appliquerGravite(env.getTerrain().getMap(), TAILLE_BLOC);
                 }
         );
 
